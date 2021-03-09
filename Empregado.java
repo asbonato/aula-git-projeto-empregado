e3b637fe75bbaaeed04bed742b7a4032084e065b
Sergio Bonato <asbonato@gmail.com>
2021-03-09 16:38:42 -0300

    Cálculo de salário efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,23 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calculaSalario() {
+        if (tipo == 1) {
+            return salario;
+        } else if (tipo == 2) {
+            // comissão varia de 0 a 1, é um percentual sobre o salário
+            return salario + salario * comissao;
+        } else if (tipo == 3) {
+            return salario + bonus;
+        } else {
+            return 0.;
+        }
+    }
 
     public String getNome() {
         return this.nome;
